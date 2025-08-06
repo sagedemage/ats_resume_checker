6ad06cf4e3438271b872ea037129bbcfd4464293
Salmaan Saeed <salsaeed999@gmail.com>
2025-08-06 01:59:28 -0400

    moved the python code to the src directory

---
+++ src/lib.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+
+class Words:
+    def __init__(
+        self, missing_words: list, most_common_words: list[tuple[Any, int]]
+    ):
+        self.missing = missing_words
+        self.most_common = most_common_words
+
+
+class Phrases:
+    def __init__(
+        self, missing_phrases: list, most_common_phrases: list[tuple[Any, int]]
+    ):
+        self.missing = missing_phrases
+        self.most_common = most_common_phrases
